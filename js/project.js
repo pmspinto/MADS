99e824eb01d051d7641d2f8f39bb319ce112e266
Tiago Almeida <ei07089@fe.up.pt>
2011-12-19 19:16:51 +0000

    Another icon (só quero fazer pull :()

--- js/project.js
+++ js/project.js
@@ -1,99 +1,43 @@
-function Project(id, name, user, creationDate, users) {
-
-	/* methods */
-
-	// saves this project instance
-	this.save = function() {
-		alert( "SAVE NOW!" )
-	}
-
-	// prints some debug info
-	this.printInfo = function(){
-		alert("id: " + this.id + 
-			"\nname: " + this.name +
-			"\nowner: " + this.owner + 
-			"\ncreation date: " + this.creationDate + 
-			"\nusers: " + this.users);
-	}
-
-	// fetch the users involved in this project
-	this.getdbProjectUsers = function(projectId){
-		return new Array("admin@mads.com", "scrub@mads.com", "lowlife@mads.com")
-	}
-
-	/* atributes */
-	this.id = id;
-	this.name = name;
-	this.owner = user;
-	this.creationDate = creationDate;
-	this.users = this.getdbProjectUsers(this.id); // array of emails used to consult the dictionary
-
-
-}
-// static methods for the Project class
-Project.staticMethod = function(){
-	alert("static method")
-}
-
-
-
-
-function ProjectList(){
-
-	/* atributes */
-	this.projects = new Object();
-
-
-	/* methods */
-	
-	// prints some debug info
-	this.printInfo = function(){
-		alert("There are " + Object.keys(this.projects).length + " projects.");
-	}
-
-	// returns a project based on the id
-	// if the project isn't found locally then it's fetched
-	this.getProject = function(id){
+function parseTasks(json, proj) {
+	rr = eval(json);
+	for(var i = 0 ; rr[i]!=null; i++) {
+		id = rr[i].id;
+		name = rr[i].name;
 		
-		// if the id exist return it
-		if(typeof this.projects[id] != 'undefined')
-			return this.projects[id];
-		else{
-			// or get it, set it here and return it	
-			dbProject = this.getdbProject(id);
-			this.projects[id] = dbProject;
-			return dbProject;
-		}	
+		user = rr[i].user;
+		idproj = rr[i].idproj;
+		idsprint = rr[i].idsprint;
+		sprintdone = rr[i].sprintdone;
+		priority = parseInt(rr[i].priority);
+		effort = parseInt(rr[i].effort);
+		t = new Task(id,name,user,idproj,idsprint,sprintdone,priority,effort);
+		proj.tasks.push(t);
 	}
+}
 
-	// returns an array of projects based on their ids
-	// if any of the projects isn't found locally then they're fetched
-	this.getProjects = function(ids){
+function Project(id) {
+	this.id = id;
 	
-		// the return value
-		returnProjects = [];		
-
-		for(id in ids){
-
-			// if the id exist return it
-			if(typeof this.projects[id] != 'undefined')
-				this.returnProjects.push(projects[id]);
-			else{
-				// or get it, set it here and return it	
-				dbProject = this.getdbProject(id);
-				this.projects[id] = dbProject;
-				returnProjects.push(dbProject);
-			}
-		}
+	this.tasks = new Array();
+	
+	this.loadProjInfo = function() {
+		console.log('loadProjInfo not done yet');
 	}
-
-	// this method is called when a project is not
-	// present in the projects list
-	// it fetches the project from the DB and
-	// returns a project object
-	this.getdbProject = function(id){
-		alert("getdbProject()");
-		return new Project(-1, "default", "default", "0000-00-00")
+	
+	this.loadProjTasks = function() {
+		$.ajax({
+			type: 'POST',
+			url: Config.server + 'ajax/getTasksProject.php',
+			data: { idproj: this.id },
+			success: function(data){
+				parseTasks(data, currentProject);
+			},
+			error: function(){
+				console.log('error in getTasksProject');
+			}
+		});
+		
+		return this.tasks;
 	}
 	
 }
