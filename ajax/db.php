3b3276585f461d4716a8d1046e0cfc6730c29b64
Pedro Pinto <ei07073@fe.up.pt>
2011-12-19 17:58:15 +0000

    cenas da db e server

--- ajax/db.php
+++ ajax/db.php
@@ -1,11 +1,11 @@
 <?php
-	/*$link = mysql_connect('gnomo.fe.up.pt', 'ei07078', 'JEuo8i2sv')
+	$link = mysql_connect('gnomo.fe.up.pt', 'ei07078', 'JEuo8i2sv')
 		or die('Could not connect: ' . mysql_error());
 	
-	mysql_select_db('ei07078') or die('Could not select database');*/
+	mysql_select_db('ei07078') or die('Could not select database');
 
-	$link = mysql_connect('gnomo.fe.up.pt', 'ei07089', 'miguelchefao007')
+	/*$link = mysql_connect('gnomo.fe.up.pt', 'ei07089', 'miguelchefao007')
 		or die('Could not connect: ' . mysql_error());
 	
-	mysql_select_db('ei07089') or die('Could not select database');
+	mysql_select_db('ei07089') or die('Could not select database');*/
 ?>
