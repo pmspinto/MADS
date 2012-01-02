67cfdc9727a3fb94743b2d651a1a9efc5e858c78
Vasco Grilo <vascogrilo@gmail.com>
2012-01-02 17:51:52 +0000

    normal changes to the db script and Config.js

--- ajax/db.php
+++ ajax/db.php
@@ -1,13 +1,20 @@
 <?php
-	/*
-	$link = mysql_connect('gnomo.fe.up.pt', 'ei07078', 'JEuo8i2sv')
+	/*$link = mysql_connect('gnomo.fe.up.pt', 'ei07078', 'JEuo8i2sv')
 		or die('Could not connect: ' . mysql_error());
 	
 	mysql_select_db('ei07078') or die('Could not select database');*/
 
 
+<<<<<<< HEAD
 	$link = mysql_connect('localhost', 'root', '')
 		or die('Could not connect: ' . mysql_error());
 	
 	mysql_select_db('test') or die('Could not select database');
 ?>
+=======
+	$link = mysql_connect('gnomo.fe.up.pt', 'ei07089', 'miguelchefao007')
+		or die('Could not connect: ' . mysql_error());
+	
+	mysql_select_db('ei07089') or die('Could not select database');
+
+>>>>>>> 147681d7745d27e4998b4ff41068529814b4a1c1
